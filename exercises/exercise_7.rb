c90dfac7e43f6552a6dc33a539d9110c36e3520d
Nahom Mehanzel <nknahom@gmail.com>
2021-09-14 20:02:56 +0000

    Add error message for invalid inputs

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,8 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Add Store name"
+store_name = gets.chomp
+puts "Store name #{store_name}"
+new_store = Store.create(name:store_name)
+puts new_store.errors.full_messages
--- lib/store.rb
+++ lib/store.rb
@@ -2,11 +2,11 @@ class Store < ActiveRecord::Base
   has_many :employees
   validates :name, length: {minimum: 3}
   validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
-  validate :must_carry_at_least_one_apparel
+  validate :must_carry_at_least_one_apparel, on: :create  
 
   def must_carry_at_least_one_apparel
     if !mens_apparel.present? && !womens_apparel.present?
-      errors.add("either mens_apparel or womens_apparel must be present")
+      errors.add(:mens_apparel,"either mens_apparel or womens_apparel must be present")
     end
   end
 end
